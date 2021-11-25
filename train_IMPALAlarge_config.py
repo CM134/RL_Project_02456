4c27405c5de4a6ba2c73c2e80463d531282064df
CM134 <cm@christian-metz.de>
2021-11-25 14:04:31 +0100

    train py main added

--- train_IMPALAlarge_config.py
+++ train_IMPALAlarge_config.py
@@ -62,23 +62,6 @@ class Flatten(nn.Module):
     def forward(self, x):
         return x.view(x.size(0), -1)
 
-
-class Encoder(nn.Module):
-  def __init__(self, in_channels, feature_dim):
-    super().__init__()
-    self.layers = nn.Sequential(
-        nn.Conv2d(in_channels=in_channels, out_channels=32, kernel_size=8, stride=4), nn.ReLU(),
-        nn.Conv2d(in_channels=32, out_channels=64, kernel_size=4, stride=2), nn.ReLU(),
-        nn.Conv2d(in_channels=64, out_channels=64, kernel_size=3, stride=1), nn.ReLU(),
-        Flatten(),
-        nn.Linear(in_features=1024, out_features=feature_dim), nn.ReLU()
-    )
-    self.apply(orthogonal_init)
-
-  def forward(self, x):
-    return self.layers(x)
-
-
 # Large IMPALA encoder 
 
 class ResidualBlock(nn.Module):
@@ -110,7 +93,7 @@ class ImpalaBlock(nn.Module):
     return x
 
 
-class ImpalaModel(nn.Module):
+class Encoder(nn.Module):
   def __init__(self, in_channels, feature_dim):
     super().__init__()
     self.imp1 = ImpalaBlock(in_channels=in_channels, out_channels=16)
@@ -160,178 +143,182 @@ class Policy(nn.Module):
 
     return dist, value
 
-
 #============================================================================================
-# Define environment
+# Main
 #============================================================================================
-# check the utils.py file for info on arguments
-env      = make_env(n_envs=num_envs, num_levels=num_levels,   env_name = envname, seed = 0, start_level=0)
-eval_env = make_env(n_envs=num_envs, num_levels=num_levels, env_name = envname, seed = 0, start_level=num_levels)
 
-encoder_in = env.observation_space.shape[0]
-num_actions = env.action_space.n
+if __name__ == "__main__":
+  #-------------------------------------------------------------------------------------------
+  # Define environment
+  #-------------------------------------------------------------------------------------------
+  # check the utils.py file for info on arguments
+  env      = make_env(n_envs=num_envs, num_levels=num_levels,   env_name = envname, seed = 0, start_level=0)
+  eval_env = make_env(n_envs=num_envs, num_levels=num_levels, env_name = envname, seed = 0, start_level=num_levels)
 
-# Define network
-encoder = ImpalaModel(in_channels=env.observation_space.shape[0], feature_dim=feature_dim)
-policy = Policy(encoder=encoder, feature_dim=feature_dim, num_actions=env.action_space.n)
-policy.cuda()
+  encoder_in = env.observation_space.shape[0]
+  num_actions = env.action_space.n
 
-# Define optimizer
-# these are reasonable values but probably not optimal
-optimizer = torch.optim.Adam(policy.parameters(), lr=learning_rate, eps=epsilon)
+  # Define network
+  encoder = Encoder(in_channels=env.observation_space.shape[0], feature_dim=feature_dim)
+  policy = Policy(encoder=encoder, feature_dim=feature_dim, num_actions=env.action_space.n)
+  policy.cuda()
 
-# Define temporary storage
-# we use this to collect transitions during each iteration
-storage = Storage(
-    env.observation_space.shape,
-    num_steps,
-    num_envs
-)
-#============================================================================================
-# Run training
-#============================================================================================
-print('Start Training...')
+  # Define optimizer
+  # these are reasonable values but probably not optimal
+  optimizer = torch.optim.Adam(policy.parameters(), lr=learning_rate, eps=epsilon)
 
-cnt_inter = 0 
-levels_played_test = 0
-level_success_test = 0
-levels_played_eval = 0
-level_success_eval = 0
-eval_reward = []
-success_rate_test_list = []
-success_rate_eval_list = []
-eval_reward_list = []
-test_reward_list = []
-step_list = []
+  # Define temporary storage
+  # we use this to collect transitions during each iteration
+  storage = Storage(
+      env.observation_space.shape,
+      num_steps,
+      num_envs
+  )
+  #-------------------------------------------------------------------------------------------
+  # Run training
+  #-------------------------------------------------------------------------------------------
+  print('Start Training...')
 
-obs = env.reset()
-eval_obs = eval_env.reset()
-step = 0
-
-while step < total_steps:
-  # ----------------------------------------------------------------
-  # Use policy to collect data for num_steps steps
-  policy.eval()
-  for _ in range(num_steps):
-    
-    # Use policy
-    action, log_prob, value = policy.act(obs)
-    
-    # Take step in environment
-    next_obs, reward, done, info = env.step(action)
-
-    for i in range(len(reward)):
-      if done[i] == 1:
-        levels_played_test = levels_played_test + 1 
-        if reward[i] > 0:
-          level_success_test = level_success_test+1
-
-    # Store data
-    storage.store(obs, action, reward, done, info, log_prob, value)
-    
-    # Update current observation
-    obs = next_obs
-
-  # Add the last observation to collected data
-  _, _, value = policy.act(obs)
-  storage.store_last(obs, value)
-
-  # Compute return and advantage
-  storage.compute_return_advantage()
-  # ----------------------------------------------------------------
-  # Eval policy
-  for _ in range(num_steps):
-    
-    # Use policy
-    action, log_prob, value = policy.act(eval_obs)
-    
-    # Take step in environment
-    eval_obs, reward, done, info = eval_env.step(action)
-    eval_reward.append(torch.Tensor(reward))
-
-    for i in range(len(reward)):
-      if done[i] == 1:
-        levels_played_eval = levels_played_eval + 1 
-        if reward[i] >0:
-          level_success_eval = level_success_eval + 1
-  # ----------------------------------------------------------------
-  # safe training and eval  
-  mean_eval_reward = torch.stack(eval_reward).sum(0).mean(0)
-  print(f'Step: {step+num_envs*num_steps}')
-  print(f'Mean test reward: {storage.get_reward()}')
-  print(f'Mean eval reward: {mean_eval_reward}')
-  print('Successful test levels:', level_success_test/levels_played_test * 100 ,'%')
-  print('Successful eval levels:', level_success_eval/levels_played_eval * 100 ,'%')
-
-  test_reward_list.append(storage.get_reward())
-  eval_reward_list.append(mean_eval_reward)
-  success_rate_test_list.append(level_success_test/levels_played_test * 100 )
-  success_rate_eval_list.append(level_success_eval/levels_played_eval * 100 )
-
+  cnt_inter = 0 
   levels_played_test = 0
   level_success_test = 0
   levels_played_eval = 0
-  level_success_eval = 0 
+  level_success_eval = 0
   eval_reward = []
+  success_rate_test_list = []
+  success_rate_eval_list = []
+  eval_reward_list = []
+  test_reward_list = []
+  step_list = []
 
-  # ----------------------------------------------------------------
-  # Optimize policy
-  policy.train()
-  for epoch in range(num_epochs):
+  obs = env.reset()
+  eval_obs = eval_env.reset()
+  step = 0
 
-    # Iterate over batches of transitions
-    generator = storage.get_generator(batch_size)
-    for batch in generator:
-      b_obs, b_action, b_log_prob, b_value, b_returns, b_advantage = batch
+  while step < total_steps:
+    # ----------------------------------------------------------------
+    # Use policy to collect data for num_steps steps
+    policy.eval()
+    for _ in range(num_steps):
+      
+      # Use policy
+      action, log_prob, value = policy.act(obs)
+      
+      # Take step in environment
+      next_obs, reward, done, info = env.step(action)
 
-      # Get current policy outputs
-      new_dist, new_value = policy(b_obs)
-      new_log_prob = new_dist.log_prob(b_action)
+      for i in range(len(reward)):
+        if done[i] == 1:
+          levels_played_test = levels_played_test + 1 
+          if reward[i] > 0:
+            level_success_test = level_success_test+1
 
-      # Clipped policy objective
-      ratio = torch.exp((new_log_prob - b_log_prob))
-      surr1 = ratio * b_advantage
-      surr2 = torch.clamp(ratio, 1.0 - ppo_eps, 1.0 + ppo_eps) * b_advantage
-      pi_loss = - torch.min(surr1, surr2).mean()
+      # Store data
+      storage.store(obs, action, reward, done, info, log_prob, value)
+      
+      # Update current observation
+      obs = next_obs
 
-      # Clipped value function objective
-      value_loss = (b_returns - new_value).pow(2).mean()
+    # Add the last observation to collected data
+    _, _, value = policy.act(obs)
+    storage.store_last(obs, value)
 
-      # Entropy loss
-      entropy_loss = new_dist.entropy()
+    # Compute return and advantage
+    storage.compute_return_advantage()
+    # ----------------------------------------------------------------
+    # Eval policy
+    for _ in range(num_steps):
+      
+      # Use policy
+      action, log_prob, value = policy.act(eval_obs)
+      
+      # Take step in environment
+      eval_obs, reward, done, info = eval_env.step(action)
+      eval_reward.append(torch.Tensor(reward))
 
-      # Backpropagate losses
-      loss = torch.mul(value_coef, value_loss) + pi_loss - torch.mul(entropy_coef, entropy_loss)
-      loss.mean().backward()
+      for i in range(len(reward)):
+        if done[i] == 1:
+          levels_played_eval = levels_played_eval + 1 
+          if reward[i] >0:
+            level_success_eval = level_success_eval + 1
+    # ----------------------------------------------------------------
+    # safe training and eval  
+    mean_eval_reward = torch.stack(eval_reward).sum(0).mean(0)
+    print(f'Step: {step+num_envs*num_steps}')
+    print(f'Mean test reward: {storage.get_reward()}')
+    print(f'Mean eval reward: {mean_eval_reward}')
+    print('Successful test levels:', level_success_test/levels_played_test * 100 ,'%')
+    print('Successful eval levels:', level_success_eval/levels_played_eval * 100 ,'%')
 
-      # Clip gradients
-      torch.nn.utils.clip_grad_norm_(policy.parameters(), grad_eps)
+    test_reward_list.append(storage.get_reward())
+    eval_reward_list.append(mean_eval_reward)
+    success_rate_test_list.append(level_success_test/levels_played_test * 100 )
+    success_rate_eval_list.append(level_success_eval/levels_played_eval * 100 )
 
-      # Update policy
-      optimizer.step()
-      optimizer.zero_grad()
+    levels_played_test = 0
+    level_success_test = 0
+    levels_played_eval = 0
+    level_success_eval = 0 
+    eval_reward = []
 
-  # Update stats
-  step += num_envs * num_steps
-  step_list.append(step)
-  
-  cnt_inter = cnt_inter + 1
-  if cnt_inter == 1:
-    torch.save(policy.state_dict(), './state_dicts/inter_' + state_dict_name)
+    # ----------------------------------------------------------------
+    # Optimize policy
+    policy.train()
+    for epoch in range(num_epochs):
+
+      # Iterate over batches of transitions
+      generator = storage.get_generator(batch_size)
+      for batch in generator:
+        b_obs, b_action, b_log_prob, b_value, b_returns, b_advantage = batch
+
+        # Get current policy outputs
+        new_dist, new_value = policy(b_obs)
+        new_log_prob = new_dist.log_prob(b_action)
+
+        # Clipped policy objective
+        ratio = torch.exp((new_log_prob - b_log_prob))
+        surr1 = ratio * b_advantage
+        surr2 = torch.clamp(ratio, 1.0 - ppo_eps, 1.0 + ppo_eps) * b_advantage
+        pi_loss = - torch.min(surr1, surr2).mean()
+
+        # Clipped value function objective
+        value_loss = (b_returns - new_value).pow(2).mean()
+
+        # Entropy loss
+        entropy_loss = new_dist.entropy()
+
+        # Backpropagate losses
+        loss = torch.mul(value_coef, value_loss) + pi_loss - torch.mul(entropy_coef, entropy_loss)
+        loss.mean().backward()
+
+        # Clip gradients
+        torch.nn.utils.clip_grad_norm_(policy.parameters(), grad_eps)
+
+        # Update policy
+        optimizer.step()
+        optimizer.zero_grad()
+
+    # Update stats
+    step += num_envs * num_steps
+    step_list.append(step)
+    
+    cnt_inter = cnt_inter + 1
+    if cnt_inter == 1:
+      torch.save(policy.state_dict(), './state_dicts/inter_' + state_dict_name)
 
 
-print('Completed training!')
-torch.save(policy.state_dict(), './state_dicts/' + state_dict_name)
+  print('Completed training!')
+  torch.save(policy.state_dict(), './state_dicts/' + state_dict_name)
 
 
-#============================================================================================
-# Safe each in csv file. 
-#============================================================================================
+  #============================================================================================
+  # Safe each in csv file. 
+  #============================================================================================
 
-import numpy as np
+  import numpy as np
 
-np.savetxt('./scores/' + outfile_name+'_test_reward.csv', test_reward_list, delimiter=', ', fmt = '% s')
-np.savetxt('./scores/' + outfile_name+'_eval_reward.csv', eval_reward_list, delimiter=', ', fmt = '% s')
-np.savetxt('./scores/' + outfile_name+'_success_rate_test.csv', success_rate_test_list, delimiter=', ', fmt = '% s')
-np.savetxt('./scores/' + outfile_name+'_success_rate_eval.csv', success_rate_eval_list, delimiter=', ', fmt = '% s')
-np.savetxt('./scores/' + outfile_name+'_steps.csv', step_list, delimiter=', ', fmt = '% s')
+  np.savetxt('./scores/' + outfile_name+'_test_reward.csv', test_reward_list, delimiter=', ', fmt = '% s')
+  np.savetxt('./scores/' + outfile_name+'_eval_reward.csv', eval_reward_list, delimiter=', ', fmt = '% s')
+  np.savetxt('./scores/' + outfile_name+'_success_rate_test.csv', success_rate_test_list, delimiter=', ', fmt = '% s')
+  np.savetxt('./scores/' + outfile_name+'_success_rate_eval.csv', success_rate_eval_list, delimiter=', ', fmt = '% s')
+  np.savetxt('./scores/' + outfile_name+'_steps.csv', step_list, delimiter=', ', fmt = '% s')
