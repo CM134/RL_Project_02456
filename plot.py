992f6b9ca580e444cd1c4d35ec7d6ccb1759b67d
CM134 <cm@christian-metz.de>
2021-11-27 14:00:39 +0100

    plot.py fixed

--- plot.py
+++ plot.py
@@ -1,3 +1,4 @@
+#%%
 import numpy as np
 # import sys
 
@@ -11,22 +12,25 @@ from numpy.lib.function_base import append
 filenames = sorted(glob.glob("./scores/*"))
 
 
-PolicyDe_rew_eval =  np.array([])
-IMPlarge_rew_eval =  np.array([])
-IMPshort_rew_eval =  np.array([])
-baseline_rew_eval =  np.array([])
-PolicyDe_rew_test =  np.array([])
-IMPlarge_rew_test =  np.array([])
-IMPshort_rew_test =  np.array([])
-baseline_rew_test =  np.array([])
-PolicyDe_rate_eval = np.array([])
-IMPlarge_rate_eval = np.array([])
-IMPshort_rate_eval = np.array([])
-baseline_rate_eval = np.array([])
-PolicyDe_rate_test = np.array([])
-IMPlarge_rate_test = np.array([])
-IMPshort_rate_test = np.array([])
-baseline_rate_test = np.array([])
+PolicyDe_rew_eval = []
+IMPlarge_rew_eval = []
+IMPshort_rew_eval = []
+baseline_rew_eval = []
+
+PolicyDe_rew_test = []
+IMPlarge_rew_test = []
+IMPshort_rew_test = []
+baseline_rew_test = []
+
+PolicyDe_rate_eval = []
+IMPlarge_rate_eval = []
+IMPshort_rate_eval = []
+baseline_rate_eval = []
+
+PolicyDe_rate_test = []
+IMPlarge_rate_test = []
+IMPshort_rate_test = []
+baseline_rate_test = []
 
 steps = []
 name = []
@@ -40,68 +44,51 @@ for fname in filenames:
     if (fname.find('large') != -1):
         if (fname.find('eval') != -1):
             if (fname.find('reward') != -1):
-              IMPlarge_rew_eval = np.append( IMPlarge_rew_eval,data)
+                IMPlarge_rew_eval.append(data)
             elif (fname.find('success_rate')):
-                IMPlarge_rate_eval.append(data)
+                IMPlarge_rate_eval.appen(data)
         elif (fname.find('test')) != -1:
             if (fname.find('reward') != -1):
-             IMPlarge_rew_test = np.append( IMPlarge_rew_test,data)
+                IMPlarge_rew_test.append(data)
             elif (fname.find('success_rate')):
-              IMPlarge_rate_test = np.append(IMPlarge_rate_test,data)
+                IMPlarge_rate_test.append(data)
 
     elif (fname.find('baseline') != -1):
         if (fname.find('eval') != -1):
             if (fname.find('reward') != -1):
-               baseline_rew_eval=np.append(baseline_rew_eval,data)
+                baseline_rew_eval.append(data)
             elif (fname.find('success_rate')):
-                baseline_rate_eval = np.append(baseline_rate_eval,data)
+                baseline_rate_eval.append(data)
         elif (fname.find('test')) != -1:
             if (fname.find('reward') != -1):
-               baseline_rew_test = np.append( baseline_rew_test,data)
+                baseline_rew_test.append(data)
             elif (fname.find('success_rate')):
-                baseline_rate_test = np.append(baseline_rate_test,data)
+                baseline_rate_test.append(data)
 
     elif (fname.find('policy') != -1):
         if (fname.find('eval') != -1):
             if (fname.find('reward') != -1):
-               PolicyDe_rew_eval = np.append( PolicyDe_rew_eval,data)            
+                PolicyDe_rew_eval.append(data)            
             elif (fname.find('success_rate')):
-                PolicyDe_rate_eval = np.append(PolicyDe_rate_eval,data)
+                PolicyDe_rate_eval.append(data)
         elif (fname.find('test')) != -1:
             if (fname.find('reward') != -1):
-               PolicyDe_rew_test = np.append( PolicyDe_rew_test,data)
+                PolicyDe_rew_test.append(data)
             elif (fname.find('success_rate')):
-                PolicyDe_rate_test = np.append(PolicyDe_rate_test,data)
+                PolicyDe_rate_test.append(data)
 
     elif (fname.find('short') != -1):
         if (fname.find('eval') != -1):
             if (fname.find('reward') != -1):
-                IMPshort_rew_eval = np.append( IMPshort_rew_eval,data)
+                IMPshort_rew_eval.append(data)
             elif (fname.find('success_rate')):
-                IMPshort_rate_eval = np.append(IMPshort_rate_eval,data)
+                IMPshort_rate_eval.append(data)
         elif (fname.find('test')) != -1:
             if (fname.find('reward') != -1):
-                IMPshort_rew_test = np.append( IMPshort_rew_test,data)
+                IMPshort_rew_test.append(data)
             elif (fname.find('success_rate')):
                 IMPshort_rate_test.append(data)
-#%%
 
-PolicyDe_rew_eval =  np.mean(np.array(PolicyDe_rew_eval ), axis = 0)
-IMPlarge_rew_eval =  np.mean(np.array(IMPlarge_rew_eval ), axis = 0)
-IMPshort_rew_eval =  np.mean(np.array(IMPshort_rew_eval ), axis = 0)
-baseline_rew_eval =  np.mean(np.array(baseline_rew_eval ), axis = 0)
-PolicyDe_rew_test =  np.mean(np.array(PolicyDe_rew_test ), axis = 0)
-IMPlarge_rew_test =  np.mean(np.array(IMPlarge_rew_test ), axis = 0)
-IMPshort_rew_test =  np.mean(np.array(IMPshort_rew_test ), axis = 0)
-baseline_rew_test =  np.mean(np.array(baseline_rew_test ), axis = 0)
-PolicyDe_rate_eval = np.mean(np.array(PolicyDe_rate_eval), axis = 0)
-IMPlarge_rate_eval = np.mean(np.array(IMPlarge_rate_eval), axis = 0)
-IMPshort_rate_eval = np.mean(np.array(IMPshort_rate_eval), axis = 0)
-baseline_rate_eval = np.mean(np.array(baseline_rate_eval), axis = 0)
-PolicyDe_rate_test = np.mean(np.array(PolicyDe_rate_test), axis = 0)
-IMPlarge_rate_test = np.mean(np.array(IMPlarge_rate_test), axis = 0)
-IMPshort_rate_test = np.mean(np.array(IMPshort_rate_test), axis = 0)
-baseline_rate_test = np.mean(np.array(baseline_rate_test), axis = 0)
 
 PolicyDe_rew_eval =  np.mean(np.array(PolicyDe_rew_eval ), axis = 0)
 IMPlarge_rew_eval =  np.mean(np.array(IMPlarge_rew_eval ), axis = 0)
@@ -124,22 +111,20 @@ rate_eval = [PolicyDe_rate_eval, IMPlarge_rate_eval, IMPshort_rate_eval, baselin
 rate_test = [PolicyDe_rate_test, IMPlarge_rate_test, IMPshort_rate_test, baseline_rate_test]
 
 #%% Plot
-# =============================================================================
-# import matplotlib.pyplot as plt
-# # if we have several runs we could do the following: https://stackoverflow.com/questions/12957582/plot-yerr-xerr-as-shaded-region-rather-than-error-bars
-# 
-# clr = ['orange','steelblue','mediumseagreen','crimson']
-# plt.figure()
-# for i in range(len(steps)):
-#     plt.plot(steps[i], rate_eval[i] , clr[i])
-# 
-# plt.legend(name)
-# # clr = ['darkorange','steelblue','seagreen']
-# for i in range(len(steps)):
-#     plt.plot(steps[i], rate_test[i], color=clr[i], linestyle='--')
-# plt.grid(True)
-# plt.xlim([0,2.5e6])
-# plt.show()
-# =============================================================================
+import matplotlib.pyplot as plt
+# if we have several runs we could do the following: https://stackoverflow.com/questions/12957582/plot-yerr-xerr-as-shaded-region-rather-than-error-bars
+
+clr = ['orange','steelblue','mediumseagreen','crimson']
+plt.figure()
+for i in range(len(steps)):
+    plt.plot(steps[i], rate_eval[i] , clr[i])
+
+plt.legend(name)
+# clr = ['darkorange','steelblue','seagreen']
+for i in range(len(steps)):
+    plt.plot(steps[i], rate_test[i], color=clr[i], linestyle='--')
+plt.grid(True)
+plt.xlim([0,2.5e6])
+plt.show()
 
 
