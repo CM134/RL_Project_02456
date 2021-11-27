831ebefc4178b31f123a851e720293d7f632adcc
CM134 <cm@christian-metz.de>
2021-11-27 12:21:43 +0100

    plot.py mean works

--- plot.py
+++ plot.py
@@ -1,4 +1,3 @@
-#%%
 import numpy as np
 # import sys
 
@@ -38,12 +37,14 @@ name = []
 
 for fname in filenames:
     data = np.loadtxt(fname, dtype=float)
+    if len(data) > 306:
+        data = data[:306]
     if (fname.find('large') != -1):
         if (fname.find('eval') != -1):
             if (fname.find('reward') != -1):
                 IMPlarge_rew_eval.append(data)
             elif (fname.find('success_rate')):
-                IMPlarge_rate_eval.appen(data)
+                IMPlarge_rate_eval.append(data)
         elif (fname.find('test')) != -1:
             if (fname.find('reward') != -1):
                 IMPlarge_rew_test.append(data)
@@ -85,42 +86,46 @@ for fname in filenames:
                 IMPshort_rew_test.append(data)
             elif (fname.find('success_rate')):
                 IMPshort_rate_test.append(data)
+#%%
+
+PolicyDe_rew_eval =  np.mean(np.array(PolicyDe_rew_eval ), axis = 0)
+IMPlarge_rew_eval =  np.mean(np.array(IMPlarge_rew_eval ), axis = 0)
+IMPshort_rew_eval =  np.mean(np.array(IMPshort_rew_eval ), axis = 0)
+baseline_rew_eval =  np.mean(np.array(baseline_rew_eval ), axis = 0)
+PolicyDe_rew_test =  np.mean(np.array(PolicyDe_rew_test ), axis = 0)
+IMPlarge_rew_test =  np.mean(np.array(IMPlarge_rew_test ), axis = 0)
+IMPshort_rew_test =  np.mean(np.array(IMPshort_rew_test ), axis = 0)
+baseline_rew_test =  np.mean(np.array(baseline_rew_test ), axis = 0)
+PolicyDe_rate_eval = np.mean(np.array(PolicyDe_rate_eval), axis = 0)
+IMPlarge_rate_eval = np.mean(np.array(IMPlarge_rate_eval), axis = 0)
+IMPshort_rate_eval = np.mean(np.array(IMPshort_rate_eval), axis = 0)
+baseline_rate_eval = np.mean(np.array(baseline_rate_eval), axis = 0)
+PolicyDe_rate_test = np.mean(np.array(PolicyDe_rate_test), axis = 0)
+IMPlarge_rate_test = np.mean(np.array(IMPlarge_rate_test), axis = 0)
+IMPshort_rate_test = np.mean(np.array(IMPshort_rate_test), axis = 0)
+baseline_rate_test = np.mean(np.array(baseline_rate_test), axis = 0)
 
 
-PolicyDe_rew_eval =  np.mean(np.array(PolicyDe_rew_eval ), axis = 1)
-IMPlarge_rew_eval =  np.mean(np.array(IMPlarge_rew_eval ), axis = 1)
-IMPshort_rew_eval =  np.mean(np.array(IMPshort_rew_eval ), axis = 1)
-baseline_rew_eval =  np.mean(np.array(baseline_rew_eval ), axis = 1)
-PolicyDe_rew_test =  np.mean(np.array(PolicyDe_rew_test ), axis = 1)
-IMPlarge_rew_test =  np.mean(np.array(IMPlarge_rew_test ), axis = 1)
-IMPshort_rew_test =  np.mean(np.array(IMPshort_rew_test ), axis = 1)
-baseline_rew_test =  np.mean(np.array(baseline_rew_test ), axis = 1)
-PolicyDe_rate_eval = np.mean(np.array(PolicyDe_rate_eval), axis = 1)
-IMPlarge_rate_eval = np.mean(np.array(IMPlarge_rate_eval), axis = 1)
-IMPshort_rate_eval = np.mean(np.array(IMPshort_rate_eval), axis = 1)
-baseline_rate_eval = np.mean(np.array(baseline_rate_eval), axis = 1)
-PolicyDe_rate_test = np.mean(np.array(PolicyDe_rate_test), axis = 1)
-IMPlarge_rate_test = np.mean(np.array(IMPlarge_rate_test), axis = 1)
-IMPshort_rate_test = np.mean(np.array(IMPshort_rate_test), axis = 1)
-baseline_rate_test = np.mean(np.array(baseline_rate_test), axis = 1)
     
         
 
 #%% Plot
-import matplotlib.pyplot as plt
-# if we have several runs we could do the following: https://stackoverflow.com/questions/12957582/plot-yerr-xerr-as-shaded-region-rather-than-error-bars
-
-clr = ['orange','steelblue','mediumseagreen','crimson']
-plt.figure()
-for i in range(len(steps)):
-    plt.plot(steps[i], rate_eval[i] , clr[i])
-
-plt.legend(name)
-# clr = ['darkorange','steelblue','seagreen']
-for i in range(len(steps)):
-    plt.plot(steps[i], rate_test[i], color=clr[i], linestyle='--')
-plt.grid(True)
-plt.xlim([0,2.5e6])
-plt.show()
+# =============================================================================
+# import matplotlib.pyplot as plt
+# # if we have several runs we could do the following: https://stackoverflow.com/questions/12957582/plot-yerr-xerr-as-shaded-region-rather-than-error-bars
+# 
+# clr = ['orange','steelblue','mediumseagreen','crimson']
+# plt.figure()
+# for i in range(len(steps)):
+#     plt.plot(steps[i], rate_eval[i] , clr[i])
+# 
+# plt.legend(name)
+# # clr = ['darkorange','steelblue','seagreen']
+# for i in range(len(steps)):
+#     plt.plot(steps[i], rate_test[i], color=clr[i], linestyle='--')
+# plt.grid(True)
+# plt.xlim([0,2.5e6])
+# plt.show()
+# =============================================================================
 
 
