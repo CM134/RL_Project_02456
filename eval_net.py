17040232b39952620e9efab45338365278fb66ff
CM134 <cm@christian-metz.de>
2021-11-29 11:39:09 +0100

    color and noise added

--- eval_net.py
+++ eval_net.py
@@ -103,13 +103,6 @@ np.savetxt('./scores/' + netname + '_background_rate.csv', mean_reward, delimite
 # Change colors
 #============================================================================================
 
-img[:,:,0] = 255
-img = eval_env.render(mode='rgb_array')
-plt.imshow(img)
-plt.show()
-
-
-
 # Make evaluation environment
 #eval_env = make_env(num_envs, num_levels=num_levels, env_name = envname, seed=2)
 eval_env = make_env(n_envs=num_envs, start_level= 10000, num_levels=1000,
@@ -137,6 +130,9 @@ for net in dictnames:
     # Evaluate policy
     policy.eval()
     for _ in range(runs):
+        # apply color changes to obs
+        obs[:,0,:,:] = obs[:,0,:,:] + 0.75
+        obs = obs/obs.max()
         # Use policy
         action, log_prob, value = policy.act(obs)
         # Take step in environment
@@ -178,5 +174,85 @@ print('Successful levels:', success_rate,'%')
 
 import numpy as np
 
-np.savetxt('./scores/' + netname + '_background_reward.csv', mean_reward, delimiter=', ', fmt = '% s')
-np.savetxt('./scores/' + netname + '_background_rate.csv', mean_reward, delimiter=', ', fmt = '% s')
+np.savetxt('./scores/' + netname + '_red_reward.csv', mean_reward, delimiter=', ', fmt = '% s')
+np.savetxt('./scores/' + netname + '_red.csv', mean_reward, delimiter=', ', fmt = '% s')
+
+
+#%% Random noise
+
+
+# Make evaluation environment
+#eval_env = make_env(num_envs, num_levels=num_levels, env_name = envname, seed=2)
+eval_env = make_env(n_envs=num_envs, start_level= 10000, num_levels=1000,
+                    env_name = envname, seed=2, use_backgrounds=False)
+
+encoder_in = eval_env.observation_space.shape[0]
+num_actions = eval_env.action_space.n
+
+# load the policy
+encoder_load = Encoder(encoder_in,feature_dim)
+policy = Policy(encoder_load,feature_dim,num_actions).cuda()
+
+level_success = 0
+levels_played = 0
+
+mean_reward = []
+success_rate = []
+
+obs = eval_env.reset()
+frames = []
+total_reward = []
+for net in dictnames:
+    # Load policy
+    policy.load_state_dict(torch.load(net))
+    # Evaluate policy
+    policy.eval()
+    for _ in range(runs):
+        # apply color changes to obs
+        d0,d1,d2,d3 = list(obs.size())
+        torch_rand = torch.from_numpy(np.random.rand(d0,d1,d2,d3)/2)
+        obs = obs + torch_rand
+        obs = obs/obs.max()
+        # Use policy
+        action, log_prob, value = policy.act(obs)
+        # Take step in environment
+        obs, reward, done, info = eval_env.step(action)
+        total_reward.append(torch.Tensor(reward))
+        # determine successful levels
+        for i in range(len(reward)):
+            if done[i] == 1:
+                levels_played = levels_played + 1 
+            if reward[i] >0:
+                level_success = level_success+1
+                
+        if make_video == True:
+            # Render environment and store
+            frame = (torch.Tensor(eval_env.render(mode='rgb_array'))*255.).byte()
+            frames.append(frame)
+    
+    if make_video == True:
+        # Save frames as video
+        frames = torch.stack(frames)
+        imageio.mimsave((vid_name + '.mp4'), frames, fps=25)
+
+    mean_reward.append(torch.stack(total_reward).sum(0).mean(0).numpy()[0])
+    success_rate.append(level_success/levels_played * 100) 
+
+    print('Eval of net done:')
+    print('Average return:', torch.stack(total_reward).sum(0).mean(0).numpy()[0])
+    print('Successful levels:', level_success/levels_played * 100,'%')
+
+    level_success = 0
+    levels_played = 0
+    total_reward = []
+
+# Calculate average return
+print('Average return:', mean_reward)
+# print('Levels played:', levels_played)
+# print('Successful level:', level_success)
+print('Successful levels:', success_rate,'%')
+
+import numpy as np
+
+np.savetxt('./scores/' + netname + '_red_reward.csv', mean_reward, delimiter=', ', fmt = '% s')
+np.savetxt('./scores/' + netname + '_red.csv', mean_reward, delimiter=', ', fmt = '% s')
--- test_env.py
+++ test_env.py
@@ -1,4 +1,4 @@
-
+#%%
 num_envs = 32
 num_levels = 1000
 
@@ -14,16 +14,104 @@ import numpy as np
 
 
 # Make evaluation environment
-#eval_env = make_env(num_envs, num_levels=num_levels, env_name = envname, seed=2)
-
-for _ in range(5):
-  eval_env = make_env(1, num_levels=1000, env_name = envname, seed=np.random.randint(0,100), use_backgrounds=True)
-  eval_env.reset()
-
-img[:,:,0] = 255
-img = eval_env.render(mode='rgb_array')
-plt.imshow(img)
+eval_env = make_env(num_envs, num_levels=num_levels, env_name = envname, seed=2, use_backgrounds=True)
+obs = eval_env.reset()
+plt.imshow(obs[0].permute(1,2,0)) 
 plt.show()
+# -------
+# red in torch
+obs = eval_env.reset()
+plt.imshow(obs[0].permute(1,2,0)) 
+plt.show()
+img = obs
+# np_rand = np.random.rand(d0,d1,d2,d3)/2
+img[:,0,:,:] = obs[:,0,:,:] + 0.75
+img_norm = img/img.max()
+img_plt = img[0].permute(1,2,0)
+print(img_plt.shape)
+plt.imshow(img_plt) 
+plt.show()
+
+d0,d1,d2,d3 = list(obs.size())
+#%% Random in torch
+obs = eval_env.reset()
+img = obs
+d0,d1,d2,d3 = list(obs.size())
+torch_rand = torch.from_numpy(np.random.rand(d0,d1,d2,d3)/2)
+img = obs + torch_rand
+img_norm = img/img.max()
+img_plt = img[0].permute(1,2,0)
+print(img_plt.shape)
+plt.imshow(img_plt) 
+plt.show()
+
+
+
+print(d0,d1,d2,d3)
+# print(np.random.rand(d0,d1,d2,d3))
+# print(obs.numpy() + np.random.rand(d0,d1,d2,d3))
+np_rand = np.random.rand(d0,d1,d2,d3)/2
+img = obs.numpy() + np_rand
+img_norm = img/img.max()
+img_plt = img[0].transpose(1,2,0)
+print(img_plt.shape)
+plt.imshow(img_plt) 
+plt.show()
+
+
+#%%red
+obs = eval_env.reset()
+plt.imshow(obs[0].permute(1,2,0)) 
+plt.show()
+# np_rand = np.random.rand(d0,d1,d2,d3)/2
+img[:,0,:,:] = obs[:,0,:,:] + 0.75
+img_norm = img/img.max()
+img_plt = img[0].permute(1,2,0)
+print(img_plt.shape)
+plt.imshow(img_plt) 
+plt.show()
+
+#%%green
+obs = eval_env.reset()
+plt.imshow(obs[0].permute(1,2,0)) 
+plt.show()
+# np_rand = np.random.rand(d0,d1,d2,d3)/2
+img[:,1,:,:] = obs[:,1,:,:].numpy() + 0.25
+img[:,2,:,:] = obs[:,2,:,:].numpy() - 0.25
+img[:,0,:,:] = obs[:,0,:,:].numpy() - 0.25
+img_norm = img/img.max()
+img_plt = img[0].transpose(1,2,0)
+print(img_plt.shape)
+plt.imshow(img_plt) 
+plt.show()
+
+#%%blue
+obs = eval_env.reset()
+plt.imshow(obs[0].permute(1,2,0)) 
+plt.show()
+# np_rand = np.random.rand(d0,d1,d2,d3)/2
+img[:,2,:,:] = obs[:,2,:,:].numpy() + 0.5
+img_norm = img/img.max()
+img_plt = img[0].transpose(1,2,0)
+print(img_plt.shape)
+plt.imshow(img_plt) 
+plt.show()
+
+# #%%
+# for _ in range(5):
+#   eval_env = make_env(1, num_levels=1000, env_name = envname, seed=np.random.randint(0,100), use_backgrounds=True)
+#   obs = eval_env.reset()
+#   print(obs.size)
+#   # print(obs)
+#   obs[0,:,:,:] = torch.from_numpy(np.uint8(obs[0,:,:,:]) + )
+#   print(obs.shape)
+#   # print(obs) 
+#   plt.show()
+
+# # img[:,:,0] = 255
+# # img = eval_env.render(mode='rgb_array')
+# # plt.imshow(img)
+# plt.show()
 
 
 
@@ -32,3 +120,5 @@ plt.show()
 
   # Render environment and store
   # frame = (torch.Tensor(eval_env.render(mode='rgb_array'))*255.).byte()
+
+# %%
