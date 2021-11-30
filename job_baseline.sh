86946d4d7a7bbef98b32778b5d53a59bd921c403
CM134 <cm@christian-metz.de>
2021-11-30 19:11:03 +0100

    preparing new file for run

--- job_baseline.sh
+++ job_baseline.sh
@@ -3,13 +3,13 @@
 ### –- specify queue --
 #BSUB -q gpuv100
 ### -- set the job Name --
-#BSUB -J 2_base
+#BSUB -J base_reg
 ### -- ask for number of cores (default: 1) --
 #BSUB -n 1
 ### -- Select the resources: 1 gpu in exclusive process mode --
 #BSUB -gpu "num=1:mode=exclusive_process"
 ### -- set walltime limit: hh:mm --  maximum 24 hours for GPU-queues right now
-#BSUB -W 10:00
+#BSUB -W 15:00
 # request GB of system-memory
 #BSUB -R "rusage[mem=10GB]"
 ### -- set the email address --
@@ -30,7 +30,8 @@
 module load numpy/1.21.1-python-3.8.11-openblas-0.3.17
 module load cuda/10.2
 
-python3 train_baseline_config.py config/conf_Baseline.json 1
-python3 train_baseline_config.py config/conf_Baseline.json 2
+python3 train_baseline_regulated_config.py config/conf_baseline_reg.json 0
+python3 train_baseline_regulated_config.py config/conf_baseline_reg.json 1
+python3 train_baseline_regulated_config.py config/conf_baseline_reg.json 2
 
 ## submit by using: bsub < jobscript.sh
